dd46412a571e2ca401ecb49dc4dbb8514c9d7570
soubhagya-kumar-behera <soubhagyabehera074@gmail.com>
2025-11-24 11:56:03 +0530

    React Program

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    if (counter >= 20){
+      console.log("Limit Exceed")
+      setCounter(counter)
+    }else {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter <= 0) {
+      console.log("Limit Exceed")
+      setCounter(counter)
+    }else {
+      setCounter(counter - 1)
+    }
+  }
+  return (
+    <>
+     <h1>Chai aur React</h1> 
+     <h2>Counter Value: {counter}</h2>
+
+     <button onClick={addValue}
+     >Add Value {counter}</button>
+     <br />
+     <button onClick={removeValue}>Remove  {counter}</button>
+
+     <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
